62ef7a8f68168ebdbf2da8f01954189fb1be27f6
Truong Gia Linh <nhoquenroi2002@gmail.com>
2022-08-11 19:34:19 +0700

    OKR

---
+++ src/main/java/org/aibles/okr_team1/OkrTeam1Application.java
@@ -0,0 +1,13 @@
+package org.aibles.okr_team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrTeam1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/aibles/okr_team1/OkrTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.okr_team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkrTeam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
